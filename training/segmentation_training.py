faf86e810f97791ac753c3de9f4d509454fd57f0
Reza Alipour <56475612+Reza-Alipour@users.noreply.github.com>
2023-12-04 23:00:57 +0330

    Minor change

--- training/segmentation_training.py
+++ training/segmentation_training.py
@@ -705,7 +705,7 @@ def main():
 
     model_cls = MaskGitTransformer if config.model.get("architecture", "transformer") == "transformer" else MaskGiTUViT
     if config.model.get("pretrained_model_path", None) is not None:
-        model = model_cls.from_pretrained('reza-alipour/mb', revision='segment',token=hf_read_token)
+        model = model_cls.from_pretrained('openMUSE/muse-256', subfolder='transformer')
     else:
         model = model_cls(**config.model.transformer)
     mask_id = model.config.mask_token_id
